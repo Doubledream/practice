5320582ffcd425b4c6e490b4ae72a3b838e18fba
zhaojun <1274802683@qq.com>
2017-04-19 20:33:38 +0800

    fix shopcart

--- sell-app/src/components/shopcart/shopcart.vue
+++ sell-app/src/components/shopcart/shopcart.vue
@@ -10,7 +10,9 @@
         <div class="price">¥0</div>
         <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right"></div>
+      <div class="content-right">
+        <div class="pay">¥{{minPrice}}元起送</div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,49 +42,59 @@
     height: 48px
     background: #141d27
     font-size: 0
-    .content-left
-      flex: 1
-      .logo-wrapper
-        display: inline-block
-        position: relative
-        top: -10px
-        margin: 0 12px
-        padding: 6px
-        width: 56px
-        height: 56px
-        box-sizing: border-box
-        vertical-align: top
-        border-radius: 50%
-        background: #141d27
-        .logo
-          width: 100%
-          height: 100%
+    .content
+      display: flex
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          vertical-align: top
           border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            text-align: center
+            background: #2b343c
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+        .price
+          display: inline-block
+          vertical-align: top
+          line-height: 24px
+          margin-top: 12px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-size: 16px
+          font-weight: 700
+          color: rgba(255, 255, 255, 0.4)
+        .desc
+          display: inline-block
+          vertical-align: top
+          line-height: 24px
+          margin: 12px 0 0 12px
+          color: rgba(255, 255, 255, 0.4)
+          font-size: 10px
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          font-size: 12px
           text-align: center
-          background: #2b343c
-          .icon-shopping_cart
-            line-height: 44px
-            font-size: 24px
-            color: #80858a
-      .price
-        display: inline-block
-        vertical-align: top
-        line-height: 24px
-        margin-top: 12px
-        padding-right: 12px
-        box-sizing: border-box
-        border-right: 1px solid rgba(255, 255, 255, 0.1)
-        font-size: 16px
-        font-weight: 700
-        color: rgba(255, 255, 255, 0.4)
-      .desc
-        display: inline-block
-        vertical-align: top
-        line-height: 24px
-        margin: 12px 0 0 12px
-        color: rgba(255, 255, 255, 0.4)
-        font-size: 10px
-    .content-right
-      flex: 0 0 105px
-      width: 105px
+          font-weight: 700
+          color: rgba(255, 255, 255, 0.4)
+          background: #2b333b
 </style>
